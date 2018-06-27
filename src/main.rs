de5cc8c2cc94c58d3b6f2b93b96b848baaab28cb
h3nnn4n <uber.renan@gmail.com>
2018-06-27 08:47:31 -0300

    Fixed T rotation matrix, WIP on the rest

--- src/main.rs
+++ src/main.rs
@@ -8,19 +8,19 @@ pub fn main() {
     let mut game = gamestate::GameState::new();
     game.set_board_size(10, 18);
     //game.spawn_random_piece();
-    game.spawn_piece(1);
+    //game.spawn_piece(1);
     //game.spawn_piece(2);
     //game.spawn_piece(3);
     //game.spawn_piece(4);
     //game.spawn_piece(5);
     //game.spawn_piece(6);
-    //game.spawn_piece(7);
+    game.spawn_piece(7);
     //game.print_board();
 
-    for _ in 0..0 {
+    for _ in 0..16 {
         print!("{}[2J", 27 as char);
         game.print_board();
-        thread::sleep(time::Duration::from_millis(160));
+        thread::sleep(time::Duration::from_millis(250));
 
         game.board.rotate_active_piece_right();
     }
