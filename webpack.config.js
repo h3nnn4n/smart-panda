02e9603b426654626fe7d61047b48ecdc2a79225
h3nnn4n <uber.renan@gmail.com>
2019-03-24 08:13:49 -0300

    Made webpack and mocha work together

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,19 @@
-/*jshint esversion: 6 */
-
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const path = require('path');
+const glob = require("glob");
+
+let entry = "./bootstrap.js";
+let outputPath = path.resolve(__dirname, "dist");
+
+if (process.env.TESTBUILD) {
+  entry = glob.sync(__dirname + "/spec/**/*.spec.js");
+  outputPath = path.resolve(__dirname, "test-dist");
+}
 
 module.exports = {
-  entry: "./bootstrap.js",
+  entry: entry,
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: outputPath,
     filename: "bootstrap.js",
   },
   mode: "development",
@@ -18,6 +25,22 @@ module.exports = {
       include: [
         path.resolve(__dirname, "js")
       ],
-    }]
+    }, {
+      test: /\.js$/,
+      exclude: ["/node_modules/"],
+      use: [{
+        loader: "babel-loader",
+        options: {
+          presets: [
+            ['@babel/preset-env', {
+              targets: "> 0.25%, not dead"
+            }]
+          ],
+        },
+      }, ],
+    }, ]
   },
+  node: {
+    fs: 'empty'
+  }
 };
