89991e43b58cd1de3f9d48b50e71f55d7a074554
h3nnn4n <uber.renan@gmail.com>
2019-03-30 12:13:12 -0300

    Improve test runner + update package.json scripts

--- spec/runner.js
+++ spec/runner.js
@@ -1,5 +1,4 @@
 import "mocha";
 
-let context = require.context('./', true, /\.spec\.js/);
+const context = require.context('./', true, /\.spec\.js/);
 context.keys().forEach(context);
-module.exports = context;
